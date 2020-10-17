daf84984dbac674a9a8a9a34c38955e6654c8a4c
AxentiAndrei2004 <71274413+AxentiAndrei2004@users.noreply.github.com>
2020-10-18 02:20:46 +0300

    Add files via upload

---
+++ Problema_6_IF.py
@@ -0,0 +1,9 @@
+a=int(input('Introduceti primul numar='))
+b=int(input('Introduceti al doilea numar='))
+c=int(input('Introduceti al treilea numar='))
+if (c>8):
+    print(a,b,c)
+if (c<8):
+    print(a)
+if (b>a):
+    print(b)
