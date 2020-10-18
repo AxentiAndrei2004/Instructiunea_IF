d97696c2375b99672937af19a8702c2507a898ac
AxentiAndrei2004 <71274413+AxentiAndrei2004@users.noreply.github.com>
2020-10-18 03:50:48 +0300

    Add files via upload

---
+++ Problema_15_IF.py
@@ -0,0 +1,11 @@
+z=int(input('locul lui Radu dupa medie='))
+if (z<=25):
+    print('A')
+if (z>25) and (z<=50):
+    print('B')
+if (z>50) and (z<=75):
+    print('C')
+if (z>75) and (z<=100):
+    print('D')
+if (z>100) and (z<=125):
+    print('E')
