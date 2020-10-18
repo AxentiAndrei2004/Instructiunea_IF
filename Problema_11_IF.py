1839479cbe018ddc7bfee45a6a971009d492ab29
AxentiAndrei2004 <71274413+AxentiAndrei2004@users.noreply.github.com>
2020-10-18 03:49:24 +0300

    Add files via upload

---
+++ Problema_11_IF.py
@@ -0,0 +1,18 @@
+a=int(input('Primul numar='))
+b=int(input('Al doilea numar='))
+c=int(input('Al treilea numar='))
+if (a%2==0):
+    print (a,'par')
+else:
+    print(a,'impar')
+if (b%2==0):
+    print (b,'par')
+else:
+    print(b,'impar')
+if (c%2==0):
+    print (a,'par')
+else:
+    print(c,'impar')
+    
+
+
