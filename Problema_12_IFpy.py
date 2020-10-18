f63e83be2aa3dfd8b1c1ec323c25b6b994ac9b92
AxentiAndrei2004 <71274413+AxentiAndrei2004@users.noreply.github.com>
2020-10-18 03:49:47 +0300

    Add files via upload

---
+++ Problema_12_IFpy.py
@@ -0,0 +1,12 @@
+y=int(input('Introduceti numarul='))
+if y%5==1:
+    print('...un pic')
+if y%5==2:
+    print('mult')
+if y%5==3:
+    print('cu pasiune')
+if y%5==4:
+    print('la nebunie')
+if y%5==5:
+    print('de loc')
+    
