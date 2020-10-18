7c67ac840c851464c20a275041b8af94bdba6adf
AxentiAndrei2004 <71274413+AxentiAndrei2004@users.noreply.github.com>
2020-10-18 22:24:00 +0300

    Add files via upload

---
+++ Problema_3_IF.py
@@ -0,0 +1,7 @@
+l=str(input('Litera:'))
+if (l=='A') or (l=='a') or (l=='e') or (l=='E') or (l=='I') or (l=='i') or (l=='O') or (l=='o') or (l=='U') or (l=='u') or (l=='Y') or (l=='y'):
+    print('Vocala')
+if or (l=='b') or (l=='B') or (l=='C') or (l=='c') or (l=='D') or (l=='d') or (l=='F') or (l=='f') or (l=='H') or (l=='h') or (l=='J') or (l=='j') or (l=='K') or (l=='k') or (l=='L') or (l=='l') or (l=='M') or (l=='m') or (l=='N') or (l=='n') or (l=='P') or (l=='p') or (l=='Q') or (l=='q') or (l=='R') or (l=='r') or (l=='s') or (l=='S') or (l=='T') or (l=='t') or (l=='V') or (l=='v') 
+    print('Consoane')
+else:
+    print('Eroare')
